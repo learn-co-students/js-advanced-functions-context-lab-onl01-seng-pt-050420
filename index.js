f963b4956d25b62234eb598abae70e8eab6ff50d
cedric-joaquin <joaquin.cedric@gmail.com>
2020-11-22 13:34:49 -0800

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,49 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+  
+function createEmployeeRecord(data) {
+    let newEmployee = {}
+    newEmployee.firstName = data[0]
+    newEmployee.familyName = data[1]
+    newEmployee.title = data[2]
+    newEmployee.payPerHour = data[3]
+    newEmployee.timeInEvents = []
+    newEmployee.timeOutEvents = []
+    return newEmployee
+}
+
+function createEmployeeRecords(data) {
+    return data.map(employee => createEmployeeRecord(employee))
+}
+
+let createTimeInEvent = function(dateStamp) {
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date: dateStamp.split(" ")[0],
+        hour: parseInt(dateStamp.split(" ")[1])
+    })
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp) {
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date: dateStamp.split(" ")[0],
+        hour: parseInt(dateStamp.split(" ")[1])
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(dateStamp) {
+    let timeIn = this.timeInEvents.find(e => e.date == dateStamp).hour
+    let timeOut = this.timeOutEvents.find(e => e.date == dateStamp).hour
+    return (timeOut - timeIn)/100
+}
+
+let wagesEarnedOnDate = function(dateStamp) {
+    return this.payPerHour * hoursWorkedOnDate.call(this, dateStamp)
+}
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
@@ -19,4 +62,14 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(e => e.firstName == firstName)
+}
+
+function calculatePayroll(employees) {
+    return  employees.reduce(function(total, employee) {
+        return total + allWagesFor.call(employee)
+    }, 0)
 }
