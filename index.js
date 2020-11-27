e034d1d76e764059a6904a36a0e58861586bb9a4
ianhutson <ih00209@georgiasouthern.edu>
2020-11-27 12:04:16 -0500

    Done.

--- index.js
+++ index.js
@@ -9,14 +9,77 @@
  for you to use if you need it!
  */
 
-let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
+let allWagesFor = function() {
+    let eligibleDates = this.timeInEvents.map(function(e) {
         return e.date
     })
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    let payable = eligibleDates.reduce(function(memo, d) {
+            return memo + wagesEarnedOnDate.call(this, d)
+        }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+// Your code here
+function createEmployeeRecord(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arr) {
+    return arr.map(function(inner_arr) {
+        return createEmployeeRecord(inner_arr)
+    })
+}
+
+function createTimeInEvent(dt) {
+    let [date, hour] = dt.split(' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+
+function createTimeOutEvent(dt) {
+    let [date, hour] = dt.split(' ')
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    let inE = this.timeInEvents.find(function(e) { return e.date === date })
+    let outE = this.timeOutEvents.find(function(e) { return e.date === date })
+    return (outE.hour - inE.hour) / 100
+}
+
+function wagesEarnedOnDate(date) {
+    let wage = hoursWorkedOnDate.call(this, date) * this.payPerHour
+    return parseFloat(wage.toString())
+}
+
+
+function findEmployeeByFirstName(array, firstName) {
+    return array.find(function(rec) {
+        return rec.firstName === firstName
+    })
+}
+
+function calculatePayroll(array) {
+    return array.reduce(function(x, r) {
+        return x + allWagesFor.call(r)
+    }, 0)
 }
