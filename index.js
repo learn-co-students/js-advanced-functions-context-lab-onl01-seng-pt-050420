945bbaef1de4236bd8aaaea92639391944ab2eb2
Jonathan East <jonathan_east@ymail.com>
2020-12-12 15:43:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,63 @@
 /* Your Code Here */
 
+let createEmployeeRecord = function(row) {
+  return {
+    firstName: row[0],
+    familyName: row[1],
+    title: row[2],
+    payPerHour: row[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
+
+let createEmployeeRecords = function(empData) {
+  return empData.map(function(row) {
+    return createEmployeeRecord(row)
+  })
+}
+
+let createTimeInEvent = function(dateStamp) {
+  let [date, hour] = dateStamp.split(" ")
+  
+  this.timeInEvents.push(
+    {
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date,
+    })
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp) {
+  let [date, hour] = dateStamp.split(" ")
+  
+  this.timeOutEvents.push(
+    {
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date,
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(foundDate) {
+  let inEvent = this.timeInEvents.find(function(event){
+    return event.date === foundDate
+  })
+  
+  let outEvent = this.timeOutEvents.find(function(event){
+    return event.date === foundDate
+  })
+  
+  return (outEvent.hour - inEvent.hour) / 100
+}
+
+let wagesEarnedOnDate = function(foundDate) {
+  let wageEarned = hoursWorkedOnDate.call(this, foundDate) * this.payPerHour
+  return parseFloat(wageEarned.toString())
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,3 +78,22 @@ let allWagesFor = function () {
 
     return payable
 }
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+  return srcArray.find(function(record){
+    return record.firstName === firstName
+  })
+}
+
+let calculatePayroll = function(empArray) {
+  return empArray.reduce(function(memo, record) {
+    return memo + allWagesFor.call(record)
+  }, 0)
+}
+
+
+
+
+
+
+
