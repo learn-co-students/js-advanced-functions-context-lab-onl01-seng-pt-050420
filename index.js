fd7f8a3a27bf0036f8bfdc3c28854ff70384b05f
ValentinaPanic <valentina.panic87@gmail.com>
2020-12-28 20:38:25 -0700

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,74 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(arr){
+    let empObj = {
+       firstName: arr[0],
+       familyName: arr[1],
+       title: arr[2],
+       payPerHour: arr[3],
+       timeInEvents: [],
+       timeOutEvents: []
+   }
+return empObj
+}
+
+function createEmployeeRecords(arr){
+  return  arr.map(createEmployeeRecord)
+}
+function createTimeInEvent(dateStamp){
+   const obj = {
+        date: dateStamp.split(" ")[0],
+        hour: parseInt(dateStamp.split(" ")[1]),
+        type: "TimeIn"
+   }
+   
+    this.timeInEvents.push(obj)
+    return this
+}
+function createTimeOutEvent(dateStamp){
+   
+    const obj = {
+        type: "TimeOut",
+        hour: parseInt(dateStamp.split(" ")[1]),
+        date: dateStamp.split(" ")[0]
+    }
+  
+  this.timeOutEvents.push(obj)
+   return this
+
+}
+
+function hoursWorkedOnDate(date){
+
+   const timeIn = this.timeInEvents.find((e) => e.date === date).hour
+   const timeOut = this.timeOutEvents.find((e) => e.date === date).hour
+   return (timeOut - timeIn)/100
+  
+}
+
+function wagesEarnedOnDate(date){
+    let payOwed = hoursWorkedOnDate.call(this, date) * this.payPerHour
+    return payOwed
+}
+
+function allWagesFor(){
+    let datesWorked = this.timeOutEvents.map(e => e.date)
+    let wagesArr = datesWorked.map(e => wagesEarnedOnDate.call(this, e))
+    let allPayOwed = wagesArr.reduce((memo, val) => memo + val, 0)
+    return allPayOwed
+}
+
+function findEmployeeByFirstName(srcArray, first_Name){
+   return srcArray.find((record) => record.firstName === first_Name)
+}
+
+function calculatePayroll(arr){
+    const payAll =  arr.map(e => allWagesFor.call(e))
+
+    return payAll.reduce(function(total, el){
+       return total + el
+   }, 0)
+}
+
